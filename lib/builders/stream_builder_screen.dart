37decce66a01a1db1dce9a2326cebf4ed32757b1
Hassanulhaq52 <Hassanulhaq52@gmail.com>
2024-02-14 11:02:31 +0500

    Stream Builder with For Loop.

--- lib/builders/stream_builder_screen.dart
+++ lib/builders/stream_builder_screen.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-
 Stream<int> generateNumbers() async* {
   int count = 0;
-  while (true) {
-    await Future.delayed(Duration(seconds: 2));
+  for (int i = 1; i<20; i++) {
+    await Future.delayed(Duration(seconds: 1));
     yield count++;
   }
 }
@@ -14,24 +13,23 @@ class StreamBuilderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('StreamBuilder Example'),
+      appBar: AppBar(
+        title: Text('StreamBuilder Example'),
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: generateNumbers(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Text('Number: ${snapshot.data}');
+            }
+          },
         ),
-        body: Center(
-          child: StreamBuilder(
-            stream: generateNumbers(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                return Text('Number: ${snapshot.data}');
-              }
-            },
-          ),
-        ),
-      );
-
+      ),
+    );
   }
 }
